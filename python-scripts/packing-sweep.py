fdd49e2da2a483547c678b4b56198154873fe981
gperaza <ggperaza@gmail.com>
2016-01-14 18:38:46 -0600

    Changes in py scripts

--- python-scripts/packing-sweep.py
+++ python-scripts/packing-sweep.py
@@ -38,7 +38,7 @@ def write_input_file(ac, seed, dirname):
 """)
 
 
-def run_simulation(ac, seed):
+def run_simulation((ac, seed)):
     reRun = False
     execPath = "/home/moukarzel/ed-disks/disks-event"
     dirname = "seed."+seed+".ac."+ac
@@ -55,7 +55,7 @@ def run_simulation(ac, seed):
     log = open(dirname + "/log", "w")
     errorLog = open(dirname + "/error_log", "w")
     print("running ac = ", ac, "seed = ", seed)
-    subprocess.check_call([execPath], cwd=dirname, stdout=log, stderr=errorLog)
+    # subprocess.check_call([execPath], cwd=dirname, stdout=log, stderr=errorLog)
     log.close()
     errorLog.close()
     print("done ac = ", ac, "seed = ", seed)
@@ -68,8 +68,8 @@ def main():
     os.chdir(sweepdir)
 
     # List of acceleration amplitudes
-    acList = ["0.5", "1.0", "2.0", "3.0", "4.0",
-              "5.0", "6.0", "7.0", "8.0", "9.0", "10.0"]
+    acList = ["00.5", "01.0", "02.0", "03.0", "04.0",
+              "05.0", "06.0", "07.0", "08.0", "09.0", "10.0"]
 
     # List of seeds.
     random.seed(123456)
